93c8012a22ffe0053fc3892b3474fbc085244acf
Mert Doğruca <a7xlife@hotmail.com>
2021-05-01 14:45:48 +0300

    home

---
+++ lib/views/home/components/search_widget.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import '../../../core/decoration/circular_border.dart';
+import '../../../core/core_shelf.dart';
+
+class SearchWidget extends StatelessWidget {
+  const SearchWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: CircularBorder().inputBorder(context),
+      child: Row(
+        children: [
+          Icon(
+            Icons.search_outlined,
+            color: context.primary,
+            size: context.width * 5.4,
+          ),
+          SizedBox(width: context.width * 1.2),
+          //TextField(decoration: TextFormDeco().pinDeco(context),)
+        ],
+      ),
+    );
+  }
+}
--- lib/views/home/home_screen.dart
+++ lib/views/home/home_screen.dart
@@ -1,7 +1,6 @@
-import 'package:activityTracer/core/core_shelf.dart';
-import 'package:activityTracer/core/decoration/circular_border.dart';
+import 'components/search_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import '../../core/core_shelf.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -16,22 +15,7 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Padding(
           padding: context.mediumPadding,
           child: Column(
-            children: [
-              Container(
-                decoration: CircularBorder().inputBorder(context),
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.search_outlined,
-                      color: context.primary,
-                      size: context.width * 5.4,
-                    ),
-                    SizedBox(width: context.width * 1.2),
-                    //TextField(decoration: TextFormDeco().pinDeco(context),)
-                  ],
-                ),
-              )
-            ],
+            children: [SearchWidget()],
           ),
         ),
       ),
