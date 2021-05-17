ab61402591c188c2f188f12b94b25bfea48b8525
Erdal Kama <erdalkama@hotmail.com>
2021-05-17 19:47:28 +0300

    Tasarımlar Eklendi

--- lib/views/home/widgets/image_box.dart
+++ lib/views/home/widgets/image_box.dart
@@ -1,6 +1,6 @@
-import 'package:activityTracer/core/init/navigation/navigation_service.dart';
-import 'package:activityTracer/views/activity/view/activity.dart';
 import 'package:flutter/material.dart';
+
+import '../../activity/view/activity.dart';
 
 class ImageBoxWidget extends StatelessWidget {
   @override
@@ -22,13 +22,41 @@ class ImageBoxWidget extends StatelessWidget {
 ClipRRect calculateImg(context) {
   return ClipRRect(
     borderRadius: BorderRadius.circular(36),
-    child: SizedBox(
-      height: 200,
-      width: double.infinity,
-      child: Image.asset(
-        'assets/logo/calculate.jpg',
-        fit: BoxFit.fitWidth,
+    child: Stack(children: [
+      SizedBox(
+        height: 200,
+        width: double.infinity,
+        child: Image.asset(
+          'assets/logo/hesap.jpg',
+          fit: BoxFit.fitWidth,
+        ),
       ),
-    ),
+      Positioned(
+        bottom: 0,
+        right: 0,
+        child: Container(
+            // ignore: sort_child_properties_last
+            child: Center(
+              child: Text(
+                'Activity Calculator',
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+            ),
+            height: 75,
+            width: 200,
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Colors.black38,
+                    Colors.black,
+                  ],
+                ),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(36),
+                ))),
+      )
+    ]),
   );
 }
--- lib/views/home/widgets/news_scroll.dart
+++ lib/views/home/widgets/news_scroll.dart
@@ -1,24 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import 'CustomPageSlider.dart';
 
 class NewsScrollWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(36),
-      child: Align(
-        heightFactor: 0.5,
-        child: ListView.builder(
-          itemCount: 2,
-          scrollDirection: Axis.horizontal,
-          shrinkWrap: true,
-          itemBuilder: (BuildContext context, int index) {
-            return Image.asset(
-              'assets/logo/calculate.jpg',
+  final List<Container> pageList = [
+    Container(
+      child: InkWell(
+        onTap: () {
+          _urlAcma(
+              'https://www.runnersworld.com/uk/news/a36373939/covid-19-running-boom/');
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(36),
+            image: const DecorationImage(
+              image: AssetImage('assets/homepictures/1.jpg'),
               fit: BoxFit.fitWidth,
-            );
-          },
+            ),
+          ),
         ),
       ),
-    );
+    ),
+    Container(
+      child: InkWell(
+        onTap: () {
+          _urlAcma(
+              'https://www.runnersworld.com/uk/news/a36417389/womens-tour-de-france/');
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(36),
+            image: const DecorationImage(
+              image: AssetImage('assets/homepictures/2.jpg'),
+              fit: BoxFit.fitWidth,
+            ),
+          ),
+        ),
+      ),
+    ),
+    Container(
+      child: InkWell(
+        onTap: () {
+          _urlAcma(
+              'https://www.runnersworld.com/uk/news/a36382081/benefits-of-low-volume-hiit-study/');
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(36),
+            image: const DecorationImage(
+              image: AssetImage('assets/homepictures/3.jpg'),
+              fit: BoxFit.fitWidth,
+            ),
+          ),
+        ),
+      ),
+    )
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+        heightFactor: 0.5,
+        alignment: Alignment.bottomCenter,
+        child: CustomSlider(pages: pageList));
+  }
+}
+
+Future _urlAcma(String link) async {
+  if (await canLaunch(link)) {
+    await launch(link);
+  } else {
+    debugPrint('Gönderdiğiniz linki açamıyorum.');
   }
 }
